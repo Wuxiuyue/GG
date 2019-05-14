8a06d527c8ee47b169208680f79e6c53837e272d
Wuxiuyue <1914403102@qq.com>
2019-05-14 12:29:41 +0800

    create 03

--- src/com/atguigu/github/GitHubTest.java
+++ src/com/atguigu/github/GitHubTest.java
@@ -3,5 +3,6 @@ package com.atguigu.github;
 public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("GitHub服务器连通");
+		System.out.println("GitHub服务器测试第二次！！");
 	}
 }
